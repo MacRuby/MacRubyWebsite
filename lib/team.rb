0f7300676d00177ce220cdaeb13ecef5f3761cdd
Richard Kilmer <rich@infoether.com>
2009-03-12 02:21:52 +0000

    finish up doc section with indexes for tutorial and recipes
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRubyWebsite/trunk@888 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/team.rb
+++ lib/team.rb
@@ -1,32 +1,36 @@
 module MacRubyTeamHelper
   
-  Member = Struct.new(:name, :affiliation, :href, :email, :summary)
+  Member = Struct.new(:handle, :name, :affiliation, :href, :email, :summary)
   
   def render_member(member)
-    html = "<p class='project_member'><strong>"
+    html = "<p class='project_member'>"
     html += (member.href ? "<a href='#{member.href}'>#{member.name}</a>" : member.name)
-    html += "</strong>"
-    html += "<span class='affiliation'>, #{member.affiliation}</span>" if member.affiliation
+    #html += "<span class='affiliation'>, #{member.affiliation}</span>" if member.affiliation
     html += "</p>"
     html
   end
   
   def team_members
     [
-      Member.new("Laurent Sansonetti", "Apple, Inc.", nil, "lsansonetti@apple.com", "Project Lead"),
-      Member.new("Rich Kilmer", "InfoEther, Inc.", "http://www.infoether.com", "rich@infoether.com", "HotCocoa"),
-      Member.new("Ben Stiglitz", "Apple, Inc.", nil, "stig@apple.com", "All things OS X"),
-      Member.new("Patrick Thomson", "Apple, Inc.", nil, "pthomson@apple.com", "All things OS X"),
-      Member.new("Eloy Duran", nil, nil, "eloy.de.enige@gmail.com", "Testing")
+      Member.new("lrz", "Laurent Sansonetti", "Apple, Inc.", nil, "lsansonetti@apple.com", "Project Lead"),
+      Member.new("rich", "Rich Kilmer", "InfoEther, Inc.", "http://www.infoether.com", "rich@infoether.com", "HotCocoa"),
+      Member.new("ben", "Ben Stiglitz", "Apple, Inc.", nil, "stig@apple.com", "All things OS X"),
+      Member.new("patrick", "Patrick Thomson", "Apple, Inc.", nil, "pthomson@apple.com", "All things OS X"),
+      Member.new("eloy", "Eloy Duran", nil, nil, "eloy.de.enige@gmail.com", "Testing"),
+      Member.new("vincent", "Vincent Isambart")
     ]
+  end
+  
+  def member_name(member_handle)
+    member = team_members.detect { |member| member.handle == member_handle }
+    member ? member.name : member_handle
   end
   
   Kudo = Struct.new(:name, :href, :reason)
   
   def render_kudo(kudo)
-    html = "<p class='project_kudo'><strong>"
+    html = "<p class='project_kudo'>"
     html += (kudo.href ? "<a href='#{kudo.href}'>#{kudo.name}</a>" : kudo.name)
-    html += "</strong>"
     html += "<br><span class='summary'>#{kudo.reason}</span>"
     html += "</p>"
     html
