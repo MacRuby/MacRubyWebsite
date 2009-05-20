832d48c7b3c34a4f35002ec7fc877ab994d0fef7
Laurent Sansonetti <lsansonetti@apple.com>
2009-05-20 21:47:29 +0000

    more events
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRubyWebsite/trunk@1580 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/events.rb
+++ lib/events.rb
@@ -4,6 +4,8 @@ module MacRubyEventsHelper
   
   def macruby_events
     [
+      Event.new("25-27 May 2009", "RailsWayCon", "http://it-republik.de/konferenzen/railswaycon/sessions/railswaycon/speaker/#3873", "Berlin, Germany", "Thilo Utke Presents MacRuby & HotCocoa"),
+      Event.new("17-19 July 2009", "RubyKaigi", "http://rubykaigi.org/2009/en", "Tokyo, Japan", "Vincent Presents MacRuby"),
       Event.new("27-29 Aug 2009", "Lone Star Ruby Conference", "", "Austin, Texas, USA", "Rich Gives a MacRuby & HotCocoa Tutorial")
     ]
   end
