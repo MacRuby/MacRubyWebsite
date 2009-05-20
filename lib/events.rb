b54fa5cb0327332247a82c63cb6a27c261838b41
Matt Aimonetti <mattaimonetti@gmail.com>
2009-05-20 07:23:01 +0000

    updated events and talks/screencasts, some refactoring would be nice :)
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRubyWebsite/trunk@1579 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/events.rb
+++ lib/events.rb
@@ -4,8 +4,6 @@ module MacRubyEventsHelper
   
   def macruby_events
     [
-      Event.new("17-18 Apr 2009", "Golden Gate Ruby Conference", "http://gogaruco.com/schedule/index.html#kilmer", "San Francisco, CA, USA", "Rich Presents MacRuby & HotCocoa"),
-      Event.new("15-16 May 2009", "Ruby on OS X", "http://www.rubyonosx.com/", "Amsterdam, Holland, The Netherlands", "Laurent &amp; Rich Presenting MacRuby and Hotcocoa"),
       Event.new("27-29 Aug 2009", "Lone Star Ruby Conference", "", "Austin, Texas, USA", "Rich Gives a MacRuby & HotCocoa Tutorial")
     ]
   end
