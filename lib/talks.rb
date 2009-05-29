1c6cbeb2425fd23888dc1b1fc7de818696b8c34f
Matt Aimonetti <mattaimonetti@gmail.com>
2009-05-29 05:13:52 +0000

    added the railswaycon talk
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRubyWebsite/trunk@1632 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/talks.rb
+++ lib/talks.rb
@@ -2,8 +2,9 @@ module MacRubyTalksHelper
   
   Talk = Struct.new(:date, :location, :name, :href, :author, :slides_href)
   
-  def macruby_screencasts
+  def macruby_talks
     [
+      Talk.new("2009-05-27", "RailsWayCon", "MacRuby & HotCocoa (slides only)",           "http://www.slideshare.net/freaklikeme/macruby-hotcocoa", "Thilo Utke", nil),
       Talk.new("2009-04-22", "GoGaruCo",  "MacRuby and HotCocoa (slides only)",           "http://www.slideshare.net/mattetti/macruby-hotcocoa-presentation-by-rich-kilmer", "Rich Kilmer", nil),
       Talk.new("2009-01-13", "SDRuby",    "MacRuby: What's the big deal?",                "http://sdruby.com/podcast/58", "Matt Aimonetti", "http://www.slideshare.net/mattetti/macruby-when-objectivec-and-ruby-meet"),
       Talk.new("2009-11-07", "RubyConf",  "OS X Application Development with HotCocoa",   "http://rubyconf2008.confreaks.com/os-x-application-development-with-hotcocoa.html", "Rich Kilmer", nil),
