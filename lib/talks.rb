64b831d787e0ab7eb3c23adcdff41ede3dc72859
Matt Aimonetti <mattaimonetti@gmail.com>
2009-07-15 20:04:43 +0000

    applied date fix, ticket #296
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRubyWebsite/trunk@2027 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/talks.rb
+++ lib/talks.rb
@@ -7,8 +7,8 @@ module MacRubyTalksHelper
       Talk.new("2009-05-27", "RailsWayCon", "MacRuby & HotCocoa (slides only)",           "http://www.slideshare.net/freaklikeme/macruby-hotcocoa", "Thilo Utke", nil),
       Talk.new("2009-04-22", "GoGaruCo",  "MacRuby and HotCocoa (slides only)",           "http://www.slideshare.net/mattetti/macruby-hotcocoa-presentation-by-rich-kilmer", "Rich Kilmer", nil),
       Talk.new("2009-01-13", "SDRuby",    "MacRuby: What's the big deal?",                "http://sdruby.com/podcast/58", "Matt Aimonetti", "http://www.slideshare.net/mattetti/macruby-when-objectivec-and-ruby-meet"),
-      Talk.new("2009-11-07", "RubyConf",  "OS X Application Development with HotCocoa",   "http://rubyconf2008.confreaks.com/os-x-application-development-with-hotcocoa.html", "Rich Kilmer", nil),
-      Talk.new("2009-11-06", "RubyConf",  "MacRuby: Ruby for your Mac",                   "http://rubyconf2008.confreaks.com/macruby-ruby-for-your-mac.html", "Laurent Sansonetti", nil)      
+      Talk.new("2008-11-07", "RubyConf",  "OS X Application Development with HotCocoa",   "http://rubyconf2008.confreaks.com/os-x-application-development-with-hotcocoa.html", "Rich Kilmer", nil),
+      Talk.new("2008-11-06", "RubyConf",  "MacRuby: Ruby for your Mac",                   "http://rubyconf2008.confreaks.com/macruby-ruby-for-your-mac.html", "Laurent Sansonetti", nil)      
     ]
   end
   
