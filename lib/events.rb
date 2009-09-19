62dd4c1acf6c3ef03a319e390ccd87d1bf577cbe
Matt Aimonetti <mattaimonetti@gmail.com>
2009-09-19 20:55:05 +0000

    updated upcoming events
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRubyWebsite/trunk@2583 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/events.rb
+++ lib/events.rb
@@ -4,9 +4,11 @@ module MacRubyEventsHelper
   
   def macruby_events
     [
-      Event.new("25-27 May 2009", "RailsWayCon", "http://it-republik.de/konferenzen/railswaycon/sessions/railswaycon/speaker/#3873", "Berlin, Germany", "Thilo Utke Presents MacRuby & HotCocoa"),
-      Event.new("17-19 July 2009", "RubyKaigi", "http://rubykaigi.org/2009/en", "Tokyo, Japan", "Vincent Presents MacRuby"),
-      Event.new("27-29 Aug 2009", "Lone Star Ruby Conference", "", "Austin, Texas, USA", "Rich Gives a MacRuby & HotCocoa Tutorial")
+      Event.new("2-3 Oct 2009", "OSDC.fr", "http://act.osdc.fr/osdc2009fr/", "Paris, France", "Olivier Gutknecht presents MacRuby"),
+      Event.new("13-14 Oct 2009", "Rails Summit Latin America", "http://www.railssummit.com.br/", "São Paulo, Brazil", "Rich Kilmer presents MacRuby"),
+      Event.new("30-31 Oct 2009", "RubyEnRails", "http://2009.rubyenrails.nl", "Amsterdam, Netherlands", "Eloy Duran Presents MacRuby & HotCocoa"),
+      Event.new("19-21 Nov 2009", "RubyConf", "http://rubyconf.org/", "San Francisco, CA", "Laurent Sansonetti presents MacRuby"),
+      Event.new("19-21 Nov 2009", "RubyConf", "http://rubyconf.org/", "San Francisco, CA", "Matt Aimonetti talks about writing 2D video games with MacRuby")
     ]
   end
   
