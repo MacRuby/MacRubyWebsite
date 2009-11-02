c3b1d5a37e441007162222c467e41c70823e5b8c
Matt Aimonetti <mattaimonetti@gmail.com>
2009-11-02 04:06:58 +0000

    updated the talks & events sections + added 3 recipes
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRubyWebsite/trunk@2937 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/events.rb
+++ lib/events.rb
@@ -4,9 +4,6 @@ module MacRubyEventsHelper
   
   def macruby_events
     [
-      Event.new("2-3 Oct 2009", "OSDC.fr", "http://act.osdc.fr/osdc2009fr/", "Paris, France", "Olivier Gutknecht presents MacRuby"),
-      Event.new("13-14 Oct 2009", "Rails Summit Latin America", "http://www.railssummit.com.br/", "São Paulo, Brazil", "Rich Kilmer presents MacRuby"),
-      Event.new("30-31 Oct 2009", "RubyEnRails", "http://2009.rubyenrails.nl", "Amsterdam, Netherlands", "Eloy Duran Presents MacRuby & HotCocoa"),
       Event.new("19-21 Nov 2009", "RubyConf", "http://rubyconf.org/", "San Francisco, CA", "Laurent Sansonetti presents MacRuby"),
       Event.new("19-21 Nov 2009", "RubyConf", "http://rubyconf.org/", "San Francisco, CA", "Matt Aimonetti talks about writing 2D video games with MacRuby")
     ]
--- lib/talks.rb
+++ lib/talks.rb
@@ -4,6 +4,9 @@ module MacRubyTalksHelper
   
   def macruby_talks
     [
+      Talk.new("2009-10-30", "RubyEnRails", "MacRuby & RubyCocoa (slides only)",          "http://tr.im/DCW0", "Eloy Duran", nil),
+      Talk.new("2009-10-14", "RailsSummit", "MacRuby", "http://agaelebe.blip.tv/file/2730084/", "Rich Kilmer", nil),
+      Talk.new("2009-10-03", "OSDC.fr", "Intro to MacRuby (slides only)", "http://www.slideshare.net/olg/macruby-an-introduction", "Olivier Gutknecht", nil),
       Talk.new("2009-05-27", "RailsWayCon", "MacRuby & HotCocoa (slides only)",           "http://www.slideshare.net/freaklikeme/macruby-hotcocoa", "Thilo Utke", nil),
       Talk.new("2009-04-22", "GoGaruCo",  "MacRuby and HotCocoa (slides only)",           "http://www.slideshare.net/mattetti/macruby-hotcocoa-presentation-by-rich-kilmer", "Rich Kilmer", nil),
       Talk.new("2009-01-13", "SDRuby",    "MacRuby: What's the big deal?",                "http://sdruby.com/podcast/58", "Matt Aimonetti", "http://www.slideshare.net/mattetti/macruby-when-objectivec-and-ruby-meet"),
