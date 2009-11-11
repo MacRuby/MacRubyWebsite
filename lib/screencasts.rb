441d1e7435f89b0489cda32563fdfe01a97338e9
Matt Aimonetti <mattaimonetti@gmail.com>
2009-11-11 08:59:02 +0000

    added Renzo's screencast to the list
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRubyWebsite/trunk@2992 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/screencasts.rb
+++ lib/screencasts.rb
@@ -4,6 +4,7 @@ module MacRubyScreencastsHelper
   
   def macruby_screencasts
     [
+      Screencast.new("2009-11-10", "Introduzione a MacRuby e HotCocoa (Italian)", "http://it.thinkcode.tv/catalogo/introduzione-a-macruby-e-hotcocoa/", "Renzo Borgatti", "ThinkCode.TV"),
       Screencast.new("2009-05-19", "Meet MacRuby", "http://peepcode.com/products/meet-macruby", "Geoffrey Grosenbach", "Peepcode"),
       Screencast.new("2009-05-19", "MacRuby Presentation (Free)", "http://nubyonrails.com/articles/macruby-presentation-from-rubyfest", "Geoffrey Grosenbach", "Peepcode"),
       Screencast.new("2009-05-05", "MacRuby - Bonus Track (Free)", "http://pragmaticstudio.com/screencasts/6-macruby", "Mike Clark", "Pragmatic Studio"),
