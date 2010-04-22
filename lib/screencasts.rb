b1aad8cbf54044a9fd1297e99011f94f6d2cfe1e
Matt Aimonetti <mattaimonetti@gmail.com>
2010-04-22 07:50:59 +0000

    added ThinkCode.TV screencast
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRubyWebsite/trunk@3957 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/screencasts.rb
+++ lib/screencasts.rb
@@ -4,6 +4,7 @@ module MacRubyScreencastsHelper
   
   def macruby_screencasts
     [
+      Screencast.new("2010-04-10", "Introduction to MacRuby and HotCocoa", "http://thinkcode.tv/catalog/introduction-macruby/", "Renzo Borgatti", "ThinkCode.TV"),
       Screencast.new("2009-11-10", "Introduzione a MacRuby e HotCocoa (Italian)", "http://it.thinkcode.tv/catalogo/introduzione-a-macruby-e-hotcocoa/", "Renzo Borgatti", "ThinkCode.TV"),
       Screencast.new("2009-05-19", "Meet MacRuby", "http://peepcode.com/products/meet-macruby", "Geoffrey Grosenbach", "Peepcode"),
       Screencast.new("2009-05-19", "MacRuby Presentation (Free)", "http://nubyonrails.com/articles/macruby-presentation-from-rubyfest", "Geoffrey Grosenbach", "Peepcode"),
