ced9fab09f6812ec7a144aca3d9dadfdb7ed4e8e
Laurent Sansonetti <lsansonetti@apple.com>
2010-05-01 01:59:31 +0000

    refreshing website
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRubyWebsite/trunk@3988 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/events.rb
+++ lib/events.rb
@@ -4,8 +4,7 @@ module MacRubyEventsHelper
   
   def macruby_events
     [
-      Event.new("19-21 Nov 2009", "RubyConf", "http://rubyconf.org/", "San Francisco, CA", "Laurent Sansonetti presents MacRuby"),
-      Event.new("19-21 Nov 2009", "RubyConf", "http://rubyconf.org/", "San Francisco, CA", "Matt Aimonetti talks about writing 2D video games with MacRuby")
+      # n/a
     ]
   end
   
